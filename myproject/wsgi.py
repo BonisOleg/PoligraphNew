647d8bfae926a3aabd21372700b107abae964019
BonisOleg <oleg@coresync.life>
2025-12-21 15:24:58 +0200

    ренде4

--- myproject/wsgi.py
+++ myproject/wsgi.py
@@ -3,8 +3,22 @@ WSGI config для myproject (compatibility layer).
 Перенаправляє на PolygraphNew.wsgi для сумісності з Render.
 """
 
+import os
+import sys
+
+# Встановлюємо DJANGO_SETTINGS_MODULE якщо не встановлено
+if 'DJANGO_SETTINGS_MODULE' not in os.environ:
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'PolygraphNew.settings.production')
+
 # Імпортуємо application з PolygraphNew
-from PolygraphNew.wsgi import application
+try:
+    from PolygraphNew.wsgi import application
+except ImportError as e:
+    # Якщо не вдалося імпортувати, спробуємо ініціалізувати Django
+    import django
+    from django.core.wsgi import get_wsgi_application
+    django.setup()
+    application = get_wsgi_application()
 
 # Експортуємо для gunicorn
 __all__ = ['application']
